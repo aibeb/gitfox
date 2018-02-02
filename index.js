99cbb0057a0d84fb7c0f22dfa449098e9c8cb07d
sun wei <sunwei@sundeMacBook-Pro.local>
2018-02-02 18:48:06 +0800

    d

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const { spawn } = require('child_process');
 
-var git = require('./lib/git');
+const argv = process.argv.slice(2);
 
-var path = require('path');
-
-var argv = process.argv.slice(2);
+console.log('parent directory is', process.cwd(), 'command is git', ...argv);
 
 if (argv.length < 1) {
-  console.log('Usage: gitfox <command> [<parent-dir>]');
+  console.log('Usage: g <command>');
   process.exit(0);
 }
 
-var command = argv.shift();
-var parent = path.resolve(process.cwd(), argv.shift() || '.');
+// 遍历当前文件夹 excluding '.' and '..'
+const files = fs.readdirSync(process.cwd());
 
-console.log('Running gitfox on ' + parent);
+files.forEach((file) => {
+  console.log(`\x1b[32m${file}\x1b[39m`);
+  const stats = fs.statSync(file);
+  if (stats.isDirectory()) {
+    if (fs.readdirSync(file).indexOf('.git') === -1) {
+      console.log('Not a GIT repository');
+    } else {
+      //
 
-git(command, parent);
+    }
+    const command = spawn('git', argv, { stdio: 'inherit' });
+    // command.stdout.pipe(process.stdout);
+    //
+    // command.stderr.pipe(process.stderr);
+    command.on('close', (code) => {
+      console.log(code);
+    });
+  } else {
+    console.log('Not a GIT repository');
+  }
+});
