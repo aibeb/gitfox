10649d00de6fa1e3ed1fd9294b3e836aae9c1d23
sun wei <sunwei@sundeMacBook-Pro.local>
2018-02-04 21:59:50 +0800

    d

--- index.js
+++ index.js
@@ -14,16 +14,21 @@ if (argv.length < 1) {
 
 // 遍历当前文件夹 excluding '.' and '..'
 fs.readdirSync(process.cwd()).map(async (file) => {
-  const stats = fs.statSync(file);
-  if (stats.isDirectory() && fs.readdirSync(file).indexOf('.git') !== -1) {
-    const { stdout, stderr } = await exec(`git ${argv.join(' ')}`, { cwd: file });
-    console.log(`\x1b[32m${file}\x1b[39m`);
-    if (stderr) {
-      console.error(`\x1b[31m${error || stderr}`);
+  try {
+    const stats = fs.statSync(file);
+    if (stats.isDirectory() && fs.readdirSync(file).indexOf('.git') !== -1) {
+      const { stdout, stderr } = await exec(`git ${argv.join(' ')}`, { cwd: file });
+      console.log(`\x1b[32m${file}\x1b[39m`);
+      if (stderr) {
+        console.error(`\x1b[31m${stderr}\x1b[31m`);
+      }
+      console.log(stdout);
+    } else {
+      console.log(`\x1b[32m${file}\x1b[39m`);
+      console.log('Not a GIT repository');
     }
-    console.log(stdout);
-  } else {
+  } catch (error) {
     console.log(`\x1b[32m${file}\x1b[39m`);
-    console.log('Not a GIT repository');
+    console.error(`\x1b[31m${error.stdout}\x1b[31m`);
   }
 });
